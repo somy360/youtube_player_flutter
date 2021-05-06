855b78699a9436f54cc3f612b666155b3081df23
Graeme Somerville <somy360@users.noreply.github.com>
2021-05-06 09:57:12 +0100

    Add files via upload

--- lib/src/player/raw_youtube_player.dart
+++ lib/src/player/raw_youtube_player.dart
@@ -77,7 +77,7 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
         key: widget.key,
         initialData: InAppWebViewInitialData(
           data: player,
-          baseUrl: Uri.parse('https://www.youtube.com'),
+          baseUrl: 'https://www.youtube.com',
           encoding: 'utf-8',
           mimeType: 'text/html',
         ),
